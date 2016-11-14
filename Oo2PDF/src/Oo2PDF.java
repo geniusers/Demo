09cabeaeeeeac89be732655c69122c6a7f68df9a
Administrator <Administrator@XXX-PC>
2016-11-14 17:38:59 +0800

    Test 4

--- Oo2PDF/src/Oo2PDF.java
+++ Oo2PDF/src/Oo2PDF.java
@@ -23,7 +23,7 @@ public class Oo2PDF {
         converter.convert(srcFile, desFile);   
         
         
-        /** File Stearm Test3
+        /** File Stearm Test4
 		DocumentFormat idf;
 		DocumentFormat odf;
 		DefaultDocumentFormatRegistry formatReg = new DefaultDocumentFormatRegistry();
